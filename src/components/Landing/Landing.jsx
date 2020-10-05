13dbdbe8e86f5fd76bed17f863a16e44a07d7e74
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-05 16:46:16 -0500

    adds font awesome icons for landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 class Landing extends Component {
   render() {
     return(
       <div className="Landing">
         <h2 className="Landing-Find">Find Your Screening Center</h2>
+        <FontAwesomeIcon
+                  icon={faEnvelope}
+                  size="1x"
+                  id="icon email-icon"
+                />
         <h2 className="Landing-Contact">Contact Info</h2>
         <h2 className="Landing-What-Now">What Now?</h2>
         <h2 className="Landing-Questions">Common Questions</h2>
