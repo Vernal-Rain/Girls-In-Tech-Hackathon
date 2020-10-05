a542a10a21842560c82039975dd5546cc7dd2d80
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-05 13:21:39 -0500

    adds landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/header';
+import Header from './Components/Header/Header';
+import Landing from './Components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Landing extends Component {
+  render() {
+    return(
+      <div className="Landing">
+        <h2 className="Landing-Find">Find Your Screening Center</h2>
+        <h2 className="Landing-Contact">Contact Info</h2>
+        <h2 className="Landing-What-Now">What Now?</h2>
+        <h2 className="Landing-Questions">Common Questions</h2>
+        <h2 className="Landing-Set-Reminder">Set Appt. Reminder</h2>
+      </div>
+    );
+  }
+}
+
+export default Landing;
