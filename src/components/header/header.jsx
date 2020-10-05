e6c86513708ac7f3d0b8e18e02b817aa32143eb4
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-05 15:56:54 -0500

    removes console error

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return(
-        <nav class="navbar navbar-dark bg-secondary">
+        <nav className="navbar navbar-dark bg-secondary">
             <div className="row col-12 d-flex justify-content-center text-white">
             <span className="h3">Breast Cancer Information App</span>
             </div>
