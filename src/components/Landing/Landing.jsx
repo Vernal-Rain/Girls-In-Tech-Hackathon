4461b678b6c557d4691f0d5dca3639cde19dc9c8
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-06 22:28:44 -0500

    Adds Container to Landing Component

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,52 +1,55 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faPhone, faArrowRight, faQuestion, faBell } from '@fortawesome/free-solid-svg-icons';
+import Container from 'react-bootstrap/Container'
 
 class Landing extends Component {
   render() {
     return(
-      <div className="Landing">
-        <h2 className="Landing-Find">
-          Find Your Screening Center
-          <FontAwesomeIcon
-                    icon={faSearch}
-                    size="1x"
-                    id="icon"
-          />
-        </h2>
-        <h2 className="Landing-Contact">
-          Contact Info
-          <FontAwesomeIcon
-                    icon={faPhone}
-                    size="1x"
-                    id="icon"
-          />
-        </h2>
-        <h2 className="Landing-What-Now">
-          What Now?
-          <FontAwesomeIcon
-                    icon={faArrowRight}
-                    size="1x"
-                    id="icon"
-          />
-        </h2>
-        <h2 className="Landing-Questions">
-          Common Questions
-          <FontAwesomeIcon
-                    icon={faQuestion}
-                    size="1x"
-                    id="icon"
-          />
-        </h2>
-        <h2 className="Landing-Set-Reminder">
-          Set Appt. Reminder
-          <FontAwesomeIcon
-                    icon={faBell}
-                    size="1x"
-                    id="icon"
-          />
-        </h2>
-      </div>
+      <Container>
+        <div className="Landing">
+          <h2 className="Landing-Find">
+            Find Your Screening Center
+            <FontAwesomeIcon
+                      icon={faSearch}
+                      size="1x"
+                      id="icon"
+            />
+          </h2>
+          <h2 className="Landing-Contact">
+            Contact Info
+            <FontAwesomeIcon
+                      icon={faPhone}
+                      size="1x"
+                      id="icon"
+            />
+          </h2>
+          <h2 className="Landing-What-Now">
+            What Now?
+            <FontAwesomeIcon
+                      icon={faArrowRight}
+                      size="1x"
+                      id="icon"
+            />
+          </h2>
+          <h2 className="Landing-Questions">
+            Common Questions
+            <FontAwesomeIcon
+                      icon={faQuestion}
+                      size="1x"
+                      id="icon"
+            />
+          </h2>
+          <h2 className="Landing-Set-Reminder">
+            Set Appt. Reminder
+            <FontAwesomeIcon
+                      icon={faBell}
+                      size="1x"
+                      id="icon"
+            />
+          </h2>
+        </div>
+      </Container>
     );
   }
 }
