82f9254ef62dea9919b5612981c68de20d9337b4
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-07 10:57:36 -0500

    Adds Location to Map Component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -36,12 +36,17 @@ class Map extends Component {
               />
               <form onSubmit={this.handleSubmit}>
                <label>
-                 Zipcode:
+                 Zipcode
                  <input type="text" value={this.state.value} onChange={this.handleChange} />
                </label>
-               <input type="submit" value="Search" />
              </form>
             </h4>
+            <h4>
+              Location
+            </h4>
+            <h5>
+              Name of Center
+            </h5>
           </Col>
           <Col>
             <div style={{width: '100%'}}>
