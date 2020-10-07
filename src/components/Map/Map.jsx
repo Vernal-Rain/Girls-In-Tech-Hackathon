6efbcc666f011485ccfbb9a6b47f4201f5da289d
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-06 22:17:08 -0500

    Adds Container to Map Component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 class Map extends Component {
   render() {
     return (
-      <div>
-      test
-     </div>
+      <Container>
+        <Row>
+          <Col>
+            i am a test
+          </Col>
+          <Col>
+            I am TEST
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
