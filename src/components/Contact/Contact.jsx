7d65621efc39ba0d8556bbe61e02de892b4bc9b3
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-07 10:52:10 -0500

    Adds Contact Component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+class Contact extends Component {
+  render() {
+    return(
+      <div>
+        <Container>
+        <h4>
+        Contact Info
+        </h4>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Contact;
