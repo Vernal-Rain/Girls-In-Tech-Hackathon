eb7feb09f44b44e47f526d93b0eaf1e541a20036
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-06 22:49:57 -0500

    Bootstraps Arrow to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends Component {
   render(){
     return(
-      <div>
-        <button>Back to Top</button>
+      <div className="fixed-bottom">
+        <FontAwesomeIcon
+                  icon={faArrowUp}
+                  size="1x"
+                  id="icon"
+                  className="float-right"
+        />
       </div>
     );
   }
