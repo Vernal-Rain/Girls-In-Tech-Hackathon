ee80b91b3ba12f4e619a53b7cf0f39dca5a8950e
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-08 11:18:32 -0500

    Removes Console Error

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -52,7 +52,7 @@ class Map extends Component {
           </Col>
           <Col>
             <div style={{width: '100%'}}>
-              <iframe title="test" width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1201%20W%2038th%20St,%20Austin,%20TX%2078705+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+              <iframe title="test" width="100%" height="600" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1201%20W%2038th%20St,%20Austin,%20TX%2078705+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
             </div>
           </Col>
         </Row>
