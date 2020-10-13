cfbe424353fd8d0df1beba4785f1bcbb070a54f6
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-13 18:04:43 -0500

    Adds .env security

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
@@ -13,6 +12,10 @@ const center = {
   lat: -3.745,
   lng: -38.523
 };
+
+const GM_API_KEY = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+
+console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
 
 function Map() {
   const [map, setMap] = React.useState(null)
@@ -34,7 +37,7 @@ function Map() {
       </Row>
       <Row>
         <LoadScript
-          googleMapsApiKey=""
+          googleMapsApiKey="GM_API_KEY"
         >
           <GoogleMap
             mapContainerStyle={containerStyle}
