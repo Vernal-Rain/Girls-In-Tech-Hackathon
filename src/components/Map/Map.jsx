e3593211e654a7d72285d3e16532dfa3995f6f79
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-13 17:31:28 -0500

    Adds React Google Maps to Map Component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,64 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+function Map() {
+  const [map, setMap] = React.useState(null)
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <h4>
-              <FontAwesomeIcon
-                        icon={faSearch}
-                        size="1x"
-                        id="icon"
-              />
-              <form onSubmit={this.handleSubmit}>
-               <label>
-                 Zipcode
-                 <input className="zipcode-input" type="text" value={this.state.value} onChange={this.handleChange} />
-               </label>
-             </form>
-            </h4>
-          <Col>
-            <h4>
-              Location
-            </h4>
-            <h5>
-              Name of Center
-            </h5>
-          </Col>
-          </Col>
-          <Col>
-            <div style={{width: '100%'}}>
-              <iframe title="test" width="100%" height="600" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1201%20W%2038th%20St,%20Austin,%20TX%2078705+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return (
+    <Container>
+      <Row>
+        future input box
+      </Row>
+      <Row>
+        <LoadScript
+          googleMapsApiKey=""
+        >
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+          </GoogleMap>
+        </LoadScript>
+      </Row>
+    </Container>
+  )
 }
 
-export default Map;
+export default React.memo(Map)
