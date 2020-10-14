9e0d6704b2944e885ef949471ae21fa99b0e973e
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-14 15:29:19 -0500

    Adds Drawers

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return(
-        <nav className="navbar navbar-dark bg-warning">
+        <nav className="navbar bg-warning">
             <div className="row col-12 d-flex justify-content-center text-white">
             <span className="h1">Breast Check</span>
             </div>
