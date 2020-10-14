64b17a23fdda7377118de8fe9613e284b6ef0a8c
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-13 19:20:19 -0500

    Adds Questions in Drawers

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Drawer from '../Drawer/Drawer';
 
-class Questions extends Component {
+class Questions extends React.Component {
+
   render() {
     return (
-      <div>
-        questions
+      <div className="AnotherComponent">
+        <h1>Welcome to my Clothing drawer</h1>
+        <Drawer title="My Clothing">
+          <ul>
+            <li>Yellow Dress</li>
+            <li>Red Skirt</li>
+            <li>Purple Jacket</li>
+          </ul>
+        </Drawer>
       </div>
     );
   }
 }
 
-export default Questions
+export default Questions;
