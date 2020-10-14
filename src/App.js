ac242652c6c0a798e2d7e69f58ee043cf7f421dd
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-14 15:59:09 -0500

    Adds react router

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Landing from './Components/Landing/Landing';
 import Map from './Components/Map/Map';
 import Footer from './Components/Footer/Footer';
 import Contact from './Components/Contact/Contact';
-import Questions from './Components/Questions/Questions'
+import Questions from './Components/Questions/Questions';
+import Error from './Components/Error/Error';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <Header />
-      <Landing />
-      <Map />
-      <Contact />
-      <Questions />
+      <Switch>
+                <Route path="/" component={Landing} exact />
+                <Route path="/Map" component={Map} />
+                <Route path="/Contact" component={Contact} />
+                <Route path="/Questions" component={Questions} />
+                <Route component={Error} />
+      </Switch>
       <Footer />
-    </div>
+    </main>
   );
 }
 
